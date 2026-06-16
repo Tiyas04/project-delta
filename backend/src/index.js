d80365a6873189c3e379d838c37f62b38fe75814
Tiyas04 <mandaltiyas2410@gmail.com>
2026-06-17 00:58:39 +0530

    Project Reinitialized

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,6 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
-connectDB()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(`MongoDB connection failed`, error);
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
